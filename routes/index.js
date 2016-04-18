961f712b8c25f0b91c1bfda10b7edecfc911b29f
Patrick Pan <patrickpan@college.harvard.edu>
2016-04-18 10:19:32 -0400

    Working on parser

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs'),
+  path = require('path')
+  cheerio = require('cheerio')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/test', (req, res) => {
+  fs.readFile(path.join("data", "sample.htm"), 'utf8', (err, data) => {
+    if(err) {
+      console.error("error reading")
+      return;
+    }
+    var d = new Date()
+    var messageData = cheerio.load(data)
+    var threads = messageData(".thread")
+    var names = threads.map((i, thread) => thread.childNodes[0].data)
+    console.log(typeof names)
+    // console.log(Object.keys(names))
+    res.end()
+  })
+})
 
 module.exports = router;
