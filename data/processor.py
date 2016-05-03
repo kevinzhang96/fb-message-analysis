dca897e9ecd05c1d0ebcb0d70adab608d1ece955
Kevin Zhang <kevin.zhang500@gmail.com>
2016-05-02 22:22:00 -0400

    fixed weird ratios bug, regenerated data using ppan's stuff

--- data/processor.py
+++ data/processor.py
@@ -2,7 +2,7 @@ import json, random, string
 from pprint import pprint
 
 with open('messages.json') as data_file:
-    data = json.load(data_file)
+    data = json.load(data_file, encoding="ISO-8859-1")
 
 func = lambda str: ''.join(random.choice(string.lowercase) for _ in xrange(len(str)))
 
