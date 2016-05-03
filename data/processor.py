fbfb161f3fe611758c3c9fcbd8a9223b6b125d0f
Kevin Zhang <kevin.zhang500@gmail.com>
2016-05-02 22:01:23 -0400

    added messages.json, processor script, and updated packages list

---
+++ data/processor.py
@@ -0,0 +1,16 @@
+import json, random, string
+from pprint import pprint
+
+with open('messages.json') as data_file:
+    data = json.load(data_file)
+
+func = lambda str: ''.join(random.choice(string.lowercase) for _ in xrange(len(str)))
+
+for key, values in data.items():
+    for val in values:
+        tokens = val['body'].split()
+        repl = map(func, tokens)
+        val['body'] = ' '.join(repl) 
+
+with open('processed.json', 'w') as out_file:
+    json.dump(data, out_file)
